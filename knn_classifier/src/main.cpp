2ec83baf6685fc5a73f3ca1cbb54b44af8d3d506
Billkyriaf <bkyriafinis@gmail.com>
2022-11-09 00:48:51 +0200

    Import data from the files

--- knn_classifier/src/main.cpp
+++ knn_classifier/src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    MNIST_Import mnist(
+            "data/train-images.idx3-ubyte",
+            "data/train-labels.idx1-ubyte",
+            "data/t10k-images.idx3-ubyte",
+            "data/t10k-labels.idx1-ubyte"
+    );
+
+    mnist.readMetadata();  // Read the metadata from the file
+    mnist.printMetadata();
+
+    // The vector to store the training images
+    std::vector<MNIST_Image *> training_images;
+    training_images.reserve(mnist.getTrDataCount());
+
+    mnist.readTrainingData(training_images);  // Read the training data
+
+    // The vector to store the test images
+    std::vector<MNIST_Image *> test_images;
+    test_images.reserve(mnist.getTsDataCount());
+
+    mnist.readTestData(test_images);  // Read the test data
     return 0;
 }
