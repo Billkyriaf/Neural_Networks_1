51ec8cd9faf3966620bcc71a389891d64200888b
Billkyriaf <bkyriafinis@gmail.com>
2022-11-09 00:49:36 +0200

    Test the data import

--- knn_classifier/src/main.cpp
+++ knn_classifier/src/main.cpp
@@ -1,4 +1,39 @@
 #include <iostream>
+#include <cmath>
+
+#include "MNIST_Import.h"
+
+
+void save_image(const ::std::string &name, std::array<uint8_t, 28 * 28> image){
+    using ::std::string;
+    using ::std::ios;
+    using ::std::ofstream;
+
+    auto as_pgm = [](const string &name) -> string {
+        if (! ((name.length() >= 4)
+               && (name.substr(name.length() - 4, 4) == ".pgm")))
+        {
+            return name + ".pgm";
+        } else {
+            return name;
+        }
+    };
+
+    ofstream out(as_pgm(name), ios::binary | ios::out | ios::trunc);
+
+    out << "P2\n28 28\n255\n";
+    for (int x = 0; x < 28; ++x) {
+        for (int y = 0; y < 28; ++y) {
+            auto pixel_val = (unsigned char)image[x * 28 + y];
+            out << (unsigned int)pixel_val;
+            out << " ";
+        }
+        out << "\n";
+    }
+
+    out.close();
+}
+
 
 int main() {
     MNIST_Import mnist(
@@ -22,5 +57,15 @@ int main() {
     test_images.reserve(mnist.getTsDataCount());
 
     mnist.readTestData(test_images);  // Read the test data
+
+    // Save the first image from the training set and the first image from the test set as pgm files to confirm that
+    // the data was read correctly
+    std::cout << "Saving training image as 'train_0.pgm'... Label: " << (int)training_images.at(0)->getLabel() << std::endl;
+    save_image("images/train_0", training_images.at(0)->getPixels());
+
+    std::cout << "Saving test image as 'test_1.pgm'... Label: " << (int)test_images.at(0)->getLabel() << std::endl;
+    save_image("images/test_0", test_images.at(0)->getPixels());
+
+    // At this point the data is stored in the vectors and the KNN algorithm can start
     return 0;
 }
